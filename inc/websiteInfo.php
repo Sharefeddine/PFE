fa5c58e39bfdd50be0bf7eca359854ca243d4523
Sharefeddine <sharefeddinearar@gmail.com>
2024-04-30 21:28:19 +0200

    init Version

---
+++ inc/websiteInfo.php
@@ -0,0 +1,20 @@
+﻿<?php
+$querySI = "SELECT * FROM `settings` where code='1' ";
+          $qSI = $conn->query($querySI);
+          $qSI->setFetchMode(PDO::FETCH_ASSOC);
+            while($row= $qSI->fetch()) {
+               $name= $row['name'];
+               $address = $row['adresse'];
+               $email = $row['email'];
+               $fix = "0".$row['fix'];
+               $fax = "0".$row['fax'];
+               $facebook=$row['facebook'];
+               $twiter=$row['twitter'];
+               $instagram=$row['instagram'];
+               $linkdin=$row['linkdin'];
+            }
+$lien = "";
+$EmailPassword = "sgvlwsqtmwlygecc";
+$facebook="";
+$twiter="";
+?>
